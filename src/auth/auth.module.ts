d97e463ea0f0e86e35acaa0979c0bf9f78cb94b5
vini <vinicius.brandaot@gmail.com>
2023-07-16 10:05:28 -0300

    feat: added jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,36 +1,34 @@
 import {
   ForbiddenException,
   Injectable,
-  NotFoundException,
-  Req,
-  Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto, SignupDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  async signin({ email, password }: AuthDto) {
+  constructor(private jwt: JwtService, private prisma: PrismaService) {}
+  async signin({ email, password }: AuthDto): Promise<string> {
     try {
       const user = await this.prisma.user.findUnique({
         where: { email },
       });
       if (!user) {
-        throw new NotFoundException('User not found');
+        throw new UnauthorizedException('incorrect user or password');
       }
 
       const testPassword = await argon.verify(user.password, password);
       console.log(testPassword);
       if (!testPassword) {
-        throw new UnauthorizedException('incorrect password');
+        throw new UnauthorizedException('incorrect user or password');
       }
-      return user;
+      return this.signtoken({ userId: user.id, email: user.email });
     } catch (error) {}
   }
-  async signup({ email, password, avatar, name }: SignupDto) {
+  async signup({ email, password, avatar, name }: SignupDto): Promise<string> {
     const hashedPassword = await argon.hash(password);
     try {
       const user = await this.prisma.user.create({
@@ -40,14 +38,24 @@ export class AuthService {
           avatar,
           name,
         },
-        select: {
-          email: true,
-          name: true,
-        },
       });
-      return user;
+      return this.signtoken({ userId: user.id, email: user.email });
     } catch (error) {
       throw new ForbiddenException('Email already in use');
     }
+  }
+
+  async signtoken({
+    userId,
+    email,
+  }: {
+    userId: number;
+    email: string;
+  }): Promise<string> {
+    const payload = { sub: userId, email };
+    return this.jwt.signAsync(payload, {
+      expiresIn: '1d',
+      secret: process.env.JWT_SECRET,
+    });
   }
 }
