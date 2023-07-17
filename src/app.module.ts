37ee4bdab2e63cbd95967dcbc3014faae6dbde2f
vini <vinicius.brandaot@gmail.com>
2023-07-17 12:14:42 -0300

    fix: removed unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PublicationModule } from './publication/publication.module';
 
@@ -14,7 +13,6 @@ import { PublicationModule } from './publication/publication.module';
       envFilePath: ['.env', '.development.env'],
       isGlobal: true,
     }),
-    UserModule,
     PrismaModule,
     PublicationModule,
   ],
